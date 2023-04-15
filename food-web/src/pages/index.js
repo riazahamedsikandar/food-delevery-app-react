96c13b515eb3ed839e53170f79a8f790f000a1d6
riaz ahamed sikandar <riazahamedsikandar@gmail.com>
2023-04-16 01:03:04 +0530

    component

---
+++ food-web/src/pages/index.js
@@ -0,0 +1,17 @@
+import Home from "../pages/HomePage/HomePage";
+import Error from "../pages/ErrorPage/ErrorPage";
+import MealDetails from "../pages/MealDetailsPage/MealDetailsPage";
+import Category from "../pages/CategoryPage/CategoryPage";
+
+export { Home, Error, MealDetails, Category };
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
